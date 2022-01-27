4e4bfec565326787f20f324ad09615d07d2d3e4d
hndrbs <hndrbudisantoso@gmail.com>
2022-01-27 11:21:45 +0700

    re init workspaces app

--- djkanban/urls.py
+++ djkanban/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls'), name='auth')
+    path('auth/', include('users.urls'), name='auth'),
+    path('workspaces/', include('workspaces.urls'), name='workspaces')
 ]
--- workspaces/apps.py
+++ workspaces/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KanbanConfig(AppConfig):
+class WorkspacesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kanban'
+    name = 'workspaces'
