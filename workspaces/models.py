24fe4d0ee5664476df87e1cdfb466f77ade61292
hndrbs <hndrbudisantoso@gmail.com>
2022-01-27 14:47:31 +0700

    add some models on kanban app

--- workspaces/models.py
+++ workspaces/models.py
@@ -5,11 +5,62 @@ from django.db import models
 class Workspace(models.Model):
   class Meta:
     db_table = 'Workspaces'
-    ordering = ['-id', '-is_active', '-updated_at', '-created_at']
+    ordering = ['-id', '-updated_at', '-created_at']
   
-  id = models.BigIntegerField(primary_key=True)
+  id = models.BigAutoField(primary_key=True)
+  title = models.CharField(max_length=100)
   desc = models.TextField(max_length=1000)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-  owner_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+  owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
   is_active = models.BooleanField()
+
+  def __str__(self) -> str:
+      return self.title
+
+class Board(models.Model):
+  class Meta:
+    db_table = 'Boards'
+    ordering = ['-id', '-updated_at', '-created_at']
+  
+  id = models.BigAutoField(primary_key=True)
+  title = models.CharField(max_length=100)
+  is_active = models.BooleanField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  workspace = models.ForeignKey('workspaces.Workspace', on_delete=models.CASCADE)
+  board_number = models.IntegerField()
+
+  def __str__(self) -> str:
+      return self.title
+
+class Card(models.Model):
+  class Meta:
+    db_table = 'Cards'
+    ordering = ['-id', '-updated_at', '-created_at']
+  
+  id = models.BigAutoField(primary_key=True)
+  title = models.CharField(max_length=100)
+  is_active = models.BooleanField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  target_date = models.DateField()
+  board = models.ForeignKey('workspaces.Board', on_delete=models.CASCADE)
+  card_number = models.IntegerField()
+
+  def __str__(self) -> str:
+      return self.title
+class Assignment(models.Model):
+  class Meta:
+    db_table = 'Assignments'
+    ordering = ['-id', '-updated_at', '-created_at']
+  
+  id = models.BigAutoField(primary_key=True)
+  is_active = models.BooleanField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  card = models.ForeignKey('workspaces.Card', on_delete=models.CASCADE)
+  user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+  
+  def __str__(self) -> str:
+      return f'{self.card} {self.user}'
