5cd8272388744568e76121f093d9e8846a113c6f
hndrbs <hndrbudisantoso@gmail.com>
2022-01-31 16:55:27 +0700

    completing edit workspace and using constants

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,5 +1,3 @@
-import base64
-from django import urls
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
@@ -8,6 +6,8 @@ from workspaces.forms import WorkspaceForm
 from .models import Workspace
 from django.contrib import messages
 from django.db.models import Q
+from .contants import Constant as Const
+from django import urls
 
 login_url = "/auth/login"
 
@@ -46,13 +46,14 @@ def add_workspace(request: HttpRequest):
         new_workspace.save()
 
         messages.success(request, "successfully save new workspace")
-        return redirect(urls.reverse("workspaces"))
+        return redirect(urls.reverse(Const.WORKSPACES_URL))
       
-      messages.warning(request, "something wrong in the data you submitted, could you please re-check it ?")
+      messages.warning(request, Const.BAD_SUBMITTED_DATA_MESSAGE)
       
     except Exception as err:
       messages.error(request, str(err))
-      messages.error(request, "something went wrong, you may try again or if error persists, cantact us")
+      # messages.error(request, "something went wrong, you may try again or if error persists, cantact us")
+      messages.error(request, Const.EXCEPTION_MESSAGE)
       
     context['form'] = form
     return render(request, 'form_workspace.html', context)
@@ -74,12 +75,33 @@ def edit_workspace(request: HttpRequest, b64_id: str) -> HttpResponse:
       return render(request, 'form_workspace.html', context)
     
     except Workspace.DoesNotExist:
-      messages.warning(request, "we cannot find your workspace, may be you've deleted it ?. please reload the page")
+      messages.warning(request, Const.NOT_FOUND_MESSAGE)
     
     except Exception as err:
       messages.error(str(err))
-      messages.error(request, "something went wrong, you may try again or if error persists, cantact us")
-      return redirect(urls.reverse('workspaces'))
+      messages.error(request, Const.EXCEPTION_MESSAGE)
+      return redirect(urls.reverse(Const.WORKSPACES_URL))
+  else:
+    try:
+      id = Workspace.get_workspace_id(b64_id)
+      workspace = Workspace.objects.get(id=id)
+      form = WorkspaceForm(request.POST)
+      if form.is_valid():
+        workspace.title = form.cleaned_data['title']
+        workspace.desc = form.cleaned_data['desc']
+        workspace.save()
+        messages.success(request, "successfully edit workspace")
+        
+        return redirect(urls.reverse(Const.WORKSPACES_URL))
+  
+      messages.warning(request, Const.BAD_SUBMITTED_DATA_MESSAGE)
+      
+    except Workspace.DoesNotExist:
+      messages.warning(request, Const.NOT_FOUND_MESSAGE)
+    
+    except Exception as err:
+      messages.error(request, str(err))
+      messages.error(request, Const.EXCEPTION_MESSAGE)
 
 @require_http_methods(["GET"])
 @login_required(login_url=login_url)
@@ -92,12 +114,12 @@ def deactivate_workspace(request: HttpRequest, b64_id: str) -> HttpResponse:
     messages.success(request, "successfully archive your workspace")
     
   except Workspace.DoesNotExist:
-    messages.warning(request, "we cannot find your workspace, may be you've deleted it ?. please reload the page")
+    messages.warning(request, Const.NOT_FOUND_MESSAGE)
 
   except Exception as err:
     messages.error(str(err))
-    messages.error(request, "something went wrong, you may try again or if error persists, cantact us")
+    messages.error(request, Const.EXCEPTION_MESSAGE)
   
-  return redirect(urls.reverse("workspaces"))
+  return redirect(urls.reverse(Const.WORKSPACES_URL))
 
       
