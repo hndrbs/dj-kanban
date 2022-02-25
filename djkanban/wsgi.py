8585d3d5dc5a4c58895680d91f2ebda60e790458
hndrbs <hndrbudisantoso@gmail.com>
2022-02-25 08:12:33 +0700

    update procfile to migrate and wsgy.py to configure whitenoise

--- djkanban/wsgi.py
+++ djkanban/wsgi.py
@@ -10,9 +10,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 import dotenv
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djkanban.settings')
 
-application = get_wsgi_application()
+application = WhiteNoise(get_wsgi_application(), root=settings.STATIC_ROOT)
