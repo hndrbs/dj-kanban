810c31f903483e010127af94f83e74353f2065b1
hndrbs <hndrbudisantoso@gmail.com>
2022-03-12 13:05:12 +0700

    fix wrong board id on moving card due to the next id already deleted

--- helpers.py
+++ helpers.py
@@ -1,9 +1,12 @@
 import base64
+from curses.ascii import isalnum
 import os
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
 from constants import Constant
+from workspaces.models import Board, Card
+from django.db.models import Max, Min
 
 def get_encryption_key():
   return os.environ.get('MODEL_ENCRYPTION_KEY')
@@ -11,6 +14,9 @@ def get_encryption_key():
 def get_model_id(encrypted_id: str) -> int:
   id_secret_key = base64.b64decode(encrypted_id).decode('utf-8')
   id = id_secret_key.split(os.environ.get('MODEL_ENCRYPTION_KEY'))[0]
+  
+  if not id.isnumeric(): return 0
+
   return int(id)
   
 def encrypt_id(id: int) -> str:
@@ -29,3 +35,10 @@ def customer_render(request: HttpRequest, template: str, context: dict) -> HttpR
 def exception_message_dispatcher(request: HttpRequest, error_message: Exception):
   messages.error(request, str(error_message))
   messages.error(request, Constant.EXCEPTION_MESSAGE)
+
+
+def get_next_board_id(board: Board) -> int:
+  return Board.objects.filter(id__gt=board.id).values_list('id').aggregate(Min('id')).get('id__min')
+
+def get_previous_board_id(board: Board) -> int:
+  return Board.objects.filter(id__lt=board.id).values_list('id').aggregate(Max('id')).get('id__max')
--- workspaces/templatetags/workspace_template_tags.py
+++ workspaces/templatetags/workspace_template_tags.py
@@ -1,7 +1,10 @@
 from django import template
-from helpers import encrypt_id
+from helpers import encrypt_id, get_next_board_id, get_previous_board_id
 
 register = template.Library()
 
 register.filter('encrypt_id', encrypt_id)
+register.filter('get_next_board_id', get_next_board_id)
+register.filter('get_previous_board_id', get_previous_board_id)
+
 
